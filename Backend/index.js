65b6a733e9818a214fdb03526e89320621211bae
rajdevsneha17 <rajdevsneha10@gmail.com>
2024-07-04 11:27:12 +0530

    aaa

--- Backend/index.js
+++ Backend/index.js
@@ -3,13 +3,13 @@ const app=express()
 const PORT=process.env.PORT||7000
 const cors = require('cors');
 app.use(cors({
-    origin: ['https://ecommercee-d1yo6tcwl-snehas-projects-f4c33c0d.vercel.app'],    
+    origin: ['https://ecommercee.vercel.app'],    
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials:true
 }));
 // Middleware to handle CORS preflight requests
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://ecommercee-d1yo6tcwl-snehas-projects-f4c33c0d.vercel.app');
+    res.header('Access-Control-Allow-Origin', 'https://ecommercee.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     if (req.method === 'OPTIONS') {
