e9ffefb7e73c01aeca95c6c71dcba01fec0bf3ab
rajdevsneha17 <rajdevsneha10@gmail.com>
2024-07-04 10:55:54 +0530

    Update index.js
    addd

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,16 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+// Middleware to handle CORS preflight requests
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://ecommercee.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
 app.use(express.json())
 
 const routes=require("./routes/sendmail")
