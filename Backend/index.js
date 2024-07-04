fb49e139a90b809d63711aef01910eed4b0c1a8a
rajdevsneha17 <rajdevsneha10@gmail.com>
2024-07-04 12:20:21 +0530

    false

--- Backend/index.js
+++ Backend/index.js
@@ -46,7 +46,7 @@ app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
+    credentials: false
 }));
 
 // Middleware to handle CORS preflight requests
