631181589d69a89826ad5ed68ed4144ef1b1bdfa
rajdevsneha17 <rajdevsneha10@gmail.com>
2024-07-04 10:17:40 +0530

    aaa

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ const app=express()
 const PORT=process.env.PORT||7000
 const cors = require('cors');
 app.use(cors({
-    origin: '*', 
+    origin: 'https://ecommerceback-five.vercel.app/',    
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
